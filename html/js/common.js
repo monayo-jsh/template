013cb7b0e5d96bda7e935d648275374dcd904b97
주상현 <monayo.jsh@gmail.com>
2023-03-30 10:55:30 +0900

    구조 변경

--- html/js/common.js
+++
@@ -1,6 +0,0 @@
-const btnSideBarClose = document.querySelector('.btn-sidebar-close'),
-      menuSidebar = document.querySelector('.menu-sidebar');
-
-btnSideBarClose.addEventListener('click', () => {
-  menuSidebar.classList.toggle('fold');
-});
